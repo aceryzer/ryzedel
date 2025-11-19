a85d1c2411734f46a318bf89580ba691ebfe7f7d
aceryzer <aceryz3r@gmail.com>
2025-11-19 18:29:35 +0500

    Create repo.js

---
+++ repo.js
@@ -0,0 +1,11 @@
+{
+  "name": "ryze logger",
+  "plugins": [
+    {
+      "name": "NoDelete by ryzer",
+      "description": "NoDelete plugin",
+      "authors": ["aceryzer"],
+      "source": "https://raw.githubusercontent.com/aceryzer/REPO/main/NoDelete_patched.js"
+    }
+  ]
+}
